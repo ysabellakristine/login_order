9c2c79c978bad0e932a935d604f5631e437213ea
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-05-19 22:44:04 +0800

    Create handleForms.php

---
+++ handleForms.php
@@ -0,0 +1,52 @@
+<?php  
+session_start();
+require_once('dbConfig.php');
+require_once('functions.php');
+
+if (isset($_POST['regBtn'])) {
+	$username = $_POST['username'];
+	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+	if(empty($username) || empty($password)) {
+		echo '<script> 
+		alert("The input field is empty!");
+		window.location.href = "register.php";
+		</script>';
+	}
+	
+	else {
+
+		if(addUser($conn, $username, $password)) {
+			header('Location: index.php');
+		}
+
+		else {
+			header('Location: register.php');
+		}
+
+	}
+}
+
+if (isset($_POST['loginBtn'])) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	if(empty($username) && empty($password)) {
+		echo "<script>
+		alert('Input fields are empty!');
+		window.location.href='index.php'
+		</script>";
+	} 
+	else {
+
+		if(login($conn, $username, $password)) {
+			header('Location: index.php');
+		}
+
+		else {
+			header('Location: login.php');
+		}
+	}
+	
+}
+?>
